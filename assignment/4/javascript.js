6d4964b1ff8835a52883ac94d4c72dd725151af6
kuldeep3 <kuldeepupreti3@gmail.com>
2020-03-11 12:33:25 +0530

    recent changes

--- assignment/4/javascript.js
+++ assignment/4/javascript.js
@@ -1,4 +1,4 @@
-function checkFieldRegistration(id){
+function checkFieldPrincipal(id){
   var fieldId=document.getElementById(id);
   var value = fieldId.value;
   var errorId='error'+id;
@@ -9,24 +9,66 @@ function checkFieldRegistration(id){
     fieldId.classList.remove("is-valid");
   }
   else if (value <= 0) {
-    errorId.innerHTML = "<div class='text-danger'>Hours should be greater than 0.</div>";
+    errorId.innerHTML = "<div class='text-danger'>Principal should be greater than 0.</div>";
     fieldId.classList.add("is-invalid");
     fieldId.classList.remove("is-valid");
   }
-  else if (value <= 40){
-   var x = fieldId.value;
-   var payment = x * 10;
-   errorId.innerHTML= " The payment is: ₹ " + payment ;
-   fieldId.classList.add("is-valid");
-   fieldId.classList.remove("is-invalid");
-   return false;
- }
- else{
-  var y = value - 40;
-  var payment = 400 + (y * 12);
-  errorId.innerHTML= " The payment is: ₹ " + payment ;
-  fieldId.classList.add("is-valid");
-  fieldId.classList.remove("is-invalid"); 
-  return true;
+  else{
+    errorId.innerHTML= "";
+    fieldId.classList.add("is-valid");
+    fieldId.classList.remove("is-invalid"); 
+    return true;
+  }
 }
+
+function checkFieldyears(id){
+  var fieldId=document.getElementById(id);
+  var value = fieldId.value;
+  var errorId='error'+id;
+  errorId=document.getElementById(errorId);
+  if (isNaN(value)) {
+    errorId.innerHTML = "<div class='text-danger'>Please enter a valid number.</div>";
+    fieldId.classList.add("is-invalid");
+    fieldId.classList.remove("is-valid");
+  }
+  else if (value <= 0) {
+    errorId.innerHTML = "<div class='text-danger'>Years should be greater than 0.</div>";
+    fieldId.classList.add("is-invalid");
+    fieldId.classList.remove("is-valid");
+  }
+  else{
+    errorId.innerHTML= "";
+    fieldId.classList.add("is-valid");
+    fieldId.classList.remove("is-invalid"); 
+    return true;
+  }
+}
+
+function checkFieldinterest(id){
+  var fieldId=document.getElementById(id);
+  var value = fieldId.value;
+  var errorId='error'+id;
+  errorId=document.getElementById(errorId);
+  if (isNaN(value)) {
+    errorId.innerHTML = "<div class='text-danger'>Please enter a valid number.</div>";
+    fieldId.classList.add("is-invalid");
+    fieldId.classList.remove("is-valid");
+  }
+  else if (value <= 0) {
+    errorId.innerHTML = "<div class='text-danger'>Interest should be greater than 0.</div>";
+    fieldId.classList.add("is-invalid");
+    fieldId.classList.remove("is-valid");
+  }
+  else{
+    errorId.innerHTML= "";
+    fieldId.classList.add("is-valid");
+    fieldId.classList.remove("is-invalid"); 
+    return true;
+  }
+}
+
+function simpleinterest() {
+  var principal = document.getElementById(Principal).value ; 
+  var years = document.getElementById(Principal).value ; 
+  var interest = document.getElementById(Principal).value;
 }
